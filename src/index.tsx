d51778f65934982a659d5792382583e4394dd7e2
AparnaNale <aparnanale14@gmail.com>
2024-08-04 19:19:02 +0530

    removed unwanted files

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Box } from '@mui/material';
 
 const root = ReactDOM.createRoot(
@@ -10,10 +7,5 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Box sx={{ backgroundColor: "grey.200", width: "100%" }}> <App /></Box>
-
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
